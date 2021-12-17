75bb0c2eb3b1525776989a15f77106477958112b
ngovanquang <banbanhbeo@outlook.com>
2021-12-17 22:12:48 +0700

    modified dust sensor

--- components/dust_sensor/dust_sensor.c
+++ components/dust_sensor/dust_sensor.c
@@ -9,7 +9,6 @@
 #include "dust_sensor.h"
 
 // == global defines ========================
-
 int PM25_GPIO = 5;
 int PM10_GPIO = 6;
 float lowRatioPM25 = 0, lowRatioPM10 = 0;
@@ -42,12 +41,9 @@ void readDustData(float *pm25, float *pm10)
 	lowRatioPM25 = mSec25/30000.0*100;
     lowRatioPM10 = mSec10/30000.0*100;
     printf("ratio25: %f, ratio10: %f\n", lowRatioPM25, lowRatioPM10);
-    if (lowRatioPM10 < 8) *pm10 = lowRatioPM10 * 10;
-    else *pm10 = lowRatioPM10*15 - 40;
+    if (lowRatioPM10 < 8) *pm10 = lowRatioPM10 * 100;
+    else *pm10 = lowRatioPM10*150 - 400;
 
-    if (lowRatioPM25 < 8) *pm10 = lowRatioPM25 * 10;
-    else *pm25 = lowRatioPM25*15 - 40;
+    if (lowRatioPM25 < 8) *pm10 = lowRatioPM25 * 100;
+    else *pm25 = lowRatioPM25*150 - 400;
 }
-
-
-// z
